3cbc7ce5294b6c3f46954603f26f5d4439a26c39
Bruce W. Herr II <bherr@indiana.edu>
2024-08-28 08:16:11 -0400

    Reformat with Black formatter

--- code/build-network.py
+++ code/build-network.py
@@ -18,23 +18,23 @@ data = requests.get(ASCTB_DATA).json()
 def get_temp_code(str):
     str = str.strip()
     str = str.lower()
-    str = re.sub('\\W+', '-', str)
-    str = re.sub('[^a-z0-9-]+', '', str)
-    return f'ASCTB-TEMP:{str}'
+    str = re.sub("\\W+", "-", str)
+    str = re.sub("[^a-z0-9-]+", "", str)
+    return f"ASCTB-TEMP:{str}"
 
 
 def get_id(item):
-    if item['id'].strip() == "":
-        return get_temp_code(item['name'])
+    if item["id"].strip() == "":
+        return get_temp_code(item["name"])
     else:
-        return item['id']
+        return item["id"]
 
 
 def get_label(item):
-    if item['rdfs_label'].strip() == "":
-        return item['name'].strip()
+    if item["rdfs_label"].strip() == "":
+        return item["name"].strip()
     else:
-        return item['rdfs_label'].strip()
+        return item["rdfs_label"].strip()
 
 
 @dataclass
@@ -56,63 +56,75 @@ def get_item(item, organ, type):
     return Item(node_id, label, type, organ, id)
 
 
-BODY = Item('UBERON:0013702', 'body', 'AS', 'body', 'UBERON:0013702')
-SYSTEMS = ['anatomical-systems', 'blood-vasculature',
-           'lymph-vasculature', 'peripheral-nervous-system',
-           'muscular-system', 'skeleton']
+BODY = Item("UBERON:0013702", "body", "AS", "body", "UBERON:0013702")
+SYSTEMS = [
+    "anatomical-systems",
+    "blood-vasculature",
+    "lymph-vasculature",
+    "peripheral-nervous-system",
+    "muscular-system",
+    "skeleton",
+]
 
-skip_organs = set(['bonemarrow-pelvis'] + SYSTEMS)
-tables = SYSTEMS + \
-    list(sorted(filter(lambda x: x not in skip_organs, data.keys())))
+skip_organs = set(["bonemarrow-pelvis"] + SYSTEMS)
+tables = SYSTEMS + list(sorted(filter(lambda x: x not in skip_organs, data.keys())))
 paths = []
 for table in tables:
     rows = data[table]
-    organ = table.replace('-', '_')
-    for row in rows['data']:
-        as_path = [get_item(item, organ, 'AS')
-                   for item in row['anatomical_structures']]
-        ct_path = [get_item(item, organ, 'CT')
-                   for item in row['cell_types']]
-        paths.append({
-            'as': as_path,
-            'ct': ct_path
-        })
+    organ = table.replace("-", "_")
+    for row in rows["data"]:
+        as_path = [get_item(item, organ, "AS") for item in row["anatomical_structures"]]
+        ct_path = [get_item(item, organ, "CT") for item in row["cell_types"]]
+        paths.append({"as": as_path, "ct": ct_path})
 
 
 tree = nx.DiGraph()
 
 
 def add_node(tree, item):
-    tree.add_node(item.id, id=item.id, label=item.name, name=item.name, type=item.type, organ=item.organ,
-                  ontology_id=item.ontology_id)
+    tree.add_node(
+        item.id,
+        id=item.id,
+        label=item.name,
+        name=item.name,
+        type=item.type,
+        organ=item.organ,
+        ontology_id=item.ontology_id,
+    )
 
 
 def add_edge(tree, source, target):
-    tree.add_edge(source.id, target.id, organ=target.organ, source=source.id,
-                  target=target.id, source_type=source.type, target_type=target.type)
+    tree.add_edge(
+        source.id,
+        target.id,
+        organ=target.organ,
+        source=source.id,
+        target=target.id,
+        source_type=source.type,
+        target_type=target.type,
+    )
 
 
 add_node(tree, BODY)
 dup = 0
 for path in paths:
     if INCLUDE_CELL_TYPES:
-        as_path = path['as'] + path['ct']
+        as_path = path["as"] + path["ct"]
     else:
-        as_path = path['as']
+        as_path = path["as"]
     if not tree.has_node(as_path[0].id):
         add_node(tree, as_path[0])
         add_edge(tree, BODY, as_path[0])
 
     for src in range(len(as_path) - 1):
         source, target = as_path[src], as_path[src + 1]
-        if source.id != target.id \
-                and not tree.has_edge(source.id, target.id):
+        if source.id != target.id and not tree.has_edge(source.id, target.id):
             if not tree.has_node(source.id):
                 add_node(tree, source)
             else:  # source node exists
                 found_child = None
                 for child in tree.successors(source.id):
-                    if tree.nodes[child]['ontology_id'] == target.ontology_id:
+                    if tree.nodes[child]["ontology_id"] == target.ontology_id:
                         target.id = child
                         found_child = True
 
@@ -126,23 +138,23 @@ for path in paths:
 
             add_edge(tree, source, target)
 
-print('is tree?', nx.is_tree(tree))
-print('has cycles?', nx.algorithms.dag.has_cycle(tree))
-print('duplicated nodes:', dup)
-nx.write_graphml_lxml(tree, 'data/asct-tree.graphml')
-nx.nx_agraph.write_dot(tree, 'data/asct-tree.dot')
-nx.nx_agraph.to_agraph(tree).draw('data/asct-tree.svg', prog='dot')
+print("is tree?", nx.is_tree(tree))
+print("has cycles?", nx.algorithms.dag.has_cycle(tree))
+print("duplicated nodes:", dup)
+nx.write_graphml_lxml(tree, "data/asct-tree.graphml")
+nx.nx_agraph.write_dot(tree, "data/asct-tree.dot")
+nx.nx_agraph.to_agraph(tree).draw("data/asct-tree.svg", prog="dot")
 
-with open('data/asct-nodes.csv', 'w', newline='') as csvfile:
-    header = ['id', 'name', 'type', 'organ', 'ontology_id']
+with open("data/asct-nodes.csv", "w", newline="") as csvfile:
+    header = ["id", "name", "type", "organ", "ontology_id"]
     writer = csv.writer(csvfile)
 
     writer.writerow(header)
     for id, data in tree.nodes.items():
         writer.writerow([data[col] for col in header])
 
-with open('data/asct-edges.csv', 'w', newline='') as csvfile:
-    header = ['organ', 'source', 'target', 'source_type', 'target_type']
+with open("data/asct-edges.csv", "w", newline="") as csvfile:
+    header = ["organ", "source", "target", "source_type", "target_type"]
     writer = csv.writer(csvfile)
 
     writer.writerow(header)
